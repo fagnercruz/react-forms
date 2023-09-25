f2fb701cfa672ea1d089e256fee13a6a52230781
Fagner Cunha da Cruz <fagnerccruz@gmail.com>
2023-09-25 20:19:07 -0300

    adicionando formulário com react-hook-form

--- src/routes/HookForm/index.tsx
+++ src/routes/HookForm/index.tsx
@@ -1,16 +1,35 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type InputsFormulario = {
+  nome: string;
+  email: string;
+  senha: string;
+};
+
 export default function HookForm() {
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log("Formulário com react-hook-form:");
-    //console.log(dadosForm);
-  }
+  const {
+    register,
+    handleSubmit,
+    /*watch,
+    formState: { errors },*/
+  } = useForm<InputsFormulario>();
+  const onSubmit: SubmitHandler<InputsFormulario> = (data) => {
+    console.log("Formulário com react-hook-form");
+    console.log(data);
+  };
+
   return (
     <>
       <h1>Formulário com react-hook-form</h1>
-      <form className="form-testes" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome" name="nome" />
-        <input type="email" placeholder="e-mail" />
-        <input type="password" placeholder="senha" />
+      <form className="form-testes" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Nome" {...register("nome")} />
+        <input
+          type="email"
+          placeholder="e-mail"
+          {...register("email" /*, { required: true }*/)}
+        />
+        {/*errors.email && <span>Este campo é obrigatório</span>*/}
+        <input type="password" placeholder="senha" {...register("senha")} />
         <button type="submit">Enviar</button>
       </form>
     </>
