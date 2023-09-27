4b213bd24a3abda04d4a84ebac84eae4df4d39d2
Fagner Cruz <fagner@sti.ufpb.br>
2023-09-27 14:07:36 -0300

    Adicionando e configurando o formulário com formik

--- src/routes/FormikForm/index.tsx
+++ src/routes/FormikForm/index.tsx
@@ -1,18 +1,49 @@
+import { Formik, Form } from "formik";
+
 export default function FormikForm() {
-  function handleSubmit(event: React.SyntheticEvent) {
-    event.preventDefault();
-    console.log("Formulário com Formik:");
-    //console.log(dadosForm);
-  }
   return (
     <>
       <h1>Formulário com Formik</h1>
-      <form className="form-testes" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome" name="nome" />
-        <input type="email" placeholder="e-mail" />
-        <input type="password" placeholder="senha" />
-        <button type="submit">Enviar</button>
-      </form>
+      <Formik
+        initialValues={{ nome: "", email: "", senha: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log("Formulário Formik");
+          console.log(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ values, handleChange, handleSubmit, handleBlur, isSubmitting }) => (
+          <Form className="form-testes" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Nome"
+              name="nome"
+              value={values.nome}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <input
+              type="email"
+              placeholder="e-mail"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <input
+              type="password"
+              placeholder="senha"
+              name="senha"
+              value={values.senha}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <button type="submit" disabled={isSubmitting}>
+              Enviar
+            </button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
